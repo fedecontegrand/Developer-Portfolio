cc71009b31326420ec5abc60606037366460b0be
Fede Conte Grand <fede.contegrand@gmail.com>
2021-11-05 13:04:14 -0300

    Updates

---
+++ precache-manifest.6bb9d75922dd6df541eb3ebb67a38e3a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786b850acb8924ce488aa4339118a536",
+    "url": "/fedesportfolio/index.html"
+  },
+  {
+    "revision": "cc91f4ea894dfcd8a626",
+    "url": "/fedesportfolio/static/css/3.ce554f52.chunk.css"
+  },
+  {
+    "revision": "08d742924f52da30f40c",
+    "url": "/fedesportfolio/static/css/4.c8ac464a.chunk.css"
+  },
+  {
+    "revision": "82558a729d518ff25a37",
+    "url": "/fedesportfolio/static/css/main.4939fb51.chunk.css"
+  },
+  {
+    "revision": "1d2ede203a788198e95b",
+    "url": "/fedesportfolio/static/js/2.bfdec0fc.chunk.js"
+  },
+  {
+    "revision": "cc91f4ea894dfcd8a626",
+    "url": "/fedesportfolio/static/js/3.20edb43c.chunk.js"
+  },
+  {
+    "revision": "08d742924f52da30f40c",
+    "url": "/fedesportfolio/static/js/4.0420f73e.chunk.js"
+  },
+  {
+    "revision": "82558a729d518ff25a37",
+    "url": "/fedesportfolio/static/js/main.d6b4882d.chunk.js"
+  },
+  {
+    "revision": "dd4979aa14e3e714963f",
+    "url": "/fedesportfolio/static/js/runtime-main.0f5acb8f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/fedesportfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/fedesportfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/fedesportfolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "f85b552f5f1779fafc20416c572e724f",
+    "url": "/fedesportfolio/static/media/bodega1.f85b552f.jpg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/fedesportfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "a41f784840703e7c39e9d5ff37133c77",
+    "url": "/fedesportfolio/static/media/developerActivity.a41f7848.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/fedesportfolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "d6a2dcd5cb04a2d44cca99b6acb5da7e",
+    "url": "/fedesportfolio/static/media/ibm_logo.d6a2dcd5.png"
+  },
+  {
+    "revision": "b1ae2adc77bafd0ab3f670d9ab06b5ca",
+    "url": "/fedesportfolio/static/media/jsFramework.b1ae2adc.svg"
+  },
+  {
+    "revision": "a15c230b794fda4eb28f2320529072a1",
+    "url": "/fedesportfolio/static/media/manOnTable.a15c230b.svg"
+  },
+  {
+    "revision": "d73bb2bcddad315c67b738e10a345582",
+    "url": "/fedesportfolio/static/media/meta_pic.d73bb2bc.png"
+  },
+  {
+    "revision": "f041d99793f0c15a757a3d8b73d937a8",
+    "url": "/fedesportfolio/static/media/pierian-data-logo.f041d997.png"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/fedesportfolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "05b89315da86d827606f827e50c8e7ca",
+    "url": "/fedesportfolio/static/media/rahulshettyacademy-logo.05b89315.png"
+  },
+  {
+    "revision": "1591c202ea86be03349f8d565ac1f959",
+    "url": "/fedesportfolio/static/media/seetest-logo.1591c202.png"
+  },
+  {
+    "revision": "96c9d7a913fc06de9bd7e13f5cb26af9",
+    "url": "/fedesportfolio/static/media/skill.96c9d7a9.svg"
+  },
+  {
+    "revision": "7ebc764bf5f86dd0e65964bc4a734411",
+    "url": "/fedesportfolio/static/media/vskills-logo.7ebc764b.png"
+  },
+  {
+    "revision": "d0a70af7f35f5943c7e28883adbbb7fd",
+    "url": "/fedesportfolio/static/media/zero-to-mastery.d0a70af7.png"
+  }
+]);
